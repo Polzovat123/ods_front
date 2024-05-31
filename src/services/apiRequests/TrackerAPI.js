0e689e072d5e38c11a6569707dcad6c9a2229a74
Polzovat123 <42420479+Polzovat123@users.noreply.github.com>
2024-05-31 23:45:09 +0500

    Update TrackerAPI.js

--- src/services/apiRequests/TrackerAPI.js
+++ src/services/apiRequests/TrackerAPI.js
@@ -4,10 +4,19 @@ const baseApi = axios.create({
     baseURL: `${process.env.REACT_APP_API_URL}/track/`,
 });
 
-export const showSwarmsOnMissions = async () => {
-    return baseApi.get(`show_swarms_on_missions`);
+// GET
+export const trackAllMissions = async () => {
+    return baseApi.get(`show_all`).then(res => res.data);
 };
 
-export const infoState = async (model) => {
-    return baseApi.get(`info_state`, model);
+export const getPercentComplete = async (mission_id) => {
+    return baseApi.get(`info_state`, { params: { mission_id } }).then(res => res.data);
+};
+
+export const getStartMap = async (mission_id, height, width) => {
+    return baseApi.get(`frame/info_state`, { params: { mission_id, height, width } }).then(res => res.data);
+};
+
+export const getCurrentMap = async (mission_id, height, width) => {
+    return baseApi.get(`frame/current_position`, { params: { mission_id, height, width } }).then(res => res.data);
 };
