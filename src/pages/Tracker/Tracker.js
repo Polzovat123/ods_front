d0d50dbac2c22f736cc373b54c3a24e6d0342b0e
Artimald <59371589+Artimald@users.noreply.github.com>
2024-06-01 12:39:49 +0500

    track

--- src/pages/Tracker/Tracker.js
+++ src/pages/Tracker/Tracker.js
@@ -1,45 +1,36 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Tracker.module.scss";
 import { trackAllMissions, getStartMap } from "../../services/apiRequests/TrackerAPI";
-import CardTracker from "../../components/CardTracker/CardsTracker";
-
+import CardTracker from "../../components/CardTracker/CardsTracker"
 const Tracker = () => {
     const [onMissions, setMissions] = useState([]);
     const [onMissionsPict, setMissionsPict] = useState([]);
 
     useEffect(() => {
-        const fetchMissionsAndImages = async () => {
-            try {
-                const missions = await trackAllMissions();
-                setMissions(missions);
-
-                const images = await Promise.all(
-                    missions.map(onMission => getStartMap(onMission.mission_id, 350, 350))
-                );
-
-                setMissionsPict(images);
-            } catch (error) {
-                console.error("Error fetching missions or images:", error);
-            }
-        };
-
-        fetchMissionsAndImages();
+        trackAllMissions()
+            .then(onMissions => {
+                setMissions(onMissions);
+            })
+            .catch(console.error)
     }, []);
+
+    onMissions.map(onMission => getStartMap(onMission.mission_id, 250, 250)
+        .then(startMap =>
+            setMissionsPict(startMap)
+        )
+        .catch(console.error)
+    )
+
+    onMissions.map(onMission => console.log(onMission))
+
+    // console.log(onMissionsPict)
 
     return (
         <div className={styles.wrapper}>
-            {onMissions.map((onMission, index) => (
-                <CardTracker 
-                    key={onMission.mission_id}
-                    mission_id={onMission.mission_id}
-                    swarm_name={onMission.swarm_name}
-                    time_start={onMission.time_start}
-                    time_finish={onMission.time_finish}
-                    initialImageObj={onMissionsPict[index]}
-                />
-            ))}
+            {
+                onMissions.map(onMission => <CardTracker swarm_name={onMission.swarm_name} time_start={onMission.time_start} time_finish={onMission.time_finish}/>)
+            }
         </div>
     );
-};
-
+}
 export default Tracker;
